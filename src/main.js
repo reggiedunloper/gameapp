2d0ca55ddcc588ef4c37cc9d52f0502ed99ec493
Denis Bernard <dbernar1@gmail.com>
2022-01-30 14:52:04 -0500

    init commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueSocketIO from 'vue-3-socket.io'
+import SocketIO from 'socket.io-client'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(new VueSocketIO({
+  debug: true,
+  connection: SocketIO('http://localhost:3000')
+}))
+
+app.mount('#app')
