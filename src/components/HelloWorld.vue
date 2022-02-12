631cedf4db21b244dc82cdc3b4a9da51dfd591b0
Denis Bernard <dbernar1@gmail.com>
2022-02-12 13:19:10 -0500

    updates

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="hello">
+
+    <input v-model="roomname" >
+    <button @click.prevent="createroom(roomname)">Create Room</button>
+
+    <p>
+    <input v-model="roomjoin" >
+    <button @click.prevent="joinroom(roomjoin)">Join Room</button>
+    </p>
+
     <h1>{{ totalCount }}</h1>
     <button @click.prevent="clickcounter">Click Counter</button>
+
+    <button @click.prevent="startgame">Start New Game</button>
+
     <div v-if="isConnected">SOCKETS CONNECTED</div>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <span>{{ user.id }} :</span> {{ user.clicks }}
+      </li>
+    </ul>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -21,7 +38,8 @@ export default {
   data () {
     return {
       isConnected: false,
-      totalCount: 0
+      totalCount: 0,
+      users: []
     }
   },
 
@@ -34,12 +52,29 @@ export default {
     },
     msg(data) {
       this.totalCount = data
+    },
+    users(users) {
+      this.users = users
+    },
+    roominfo(data) {
+      console.log(data)
     }
   },
 
   methods: {
     clickcounter() {
       this.$socket.emit('msg', 'clicked');
+    },
+    startgame() {
+      this.$socket.emit('startgame', 'new game start');
+    },
+    createroom(roomname) {
+      console.log(roomname);
+      this.$socket.emit('createroom', roomname);
+    },
+    joinroom(roomjoin) {
+      console.log(roomjoin);
+      this.$socket.emit('joinroom', roomjoin);
     }
   }
 }
